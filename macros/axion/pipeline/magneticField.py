15111d2959fe0b22c377e5ba8bb4a2acad31b2e0
Javier Galan <j.a.galan.81@gmail.com>
2020-03-06 11:21:28 +0100

    pipeline/magneticField.py added.

---
+++ macros/axion/pipeline/magneticField.py
@@ -0,0 +1,30 @@
+import ROOT
+
+ROOT.gSystem.Load("libGdml.so")
+ROOT.gSystem.Load("libRestCore.so")
+ROOT.gSystem.Load("libRestEvents.so")
+ROOT.gSystem.Load("libRestMetadata.so")
+ROOT.gSystem.Load("libRestAxion.so")
+
+myField = ROOT.TRestAxionMagneticField("metadata.rml", "bFieldBabyIAXO")
+
+p1 = ROOT.TVector3(0,0,0)
+p2 = ROOT.TVector3(0,0,-2500)
+p3 = ROOT.TVector3(0,0,2500)
+p4 = ROOT.TVector3(0,0,4500)
+d1 = ROOT.TVector3(0,0,1)
+
+b1 = int( 1000 * myField.GetTransversalComponent( p1, d1))
+b2 = int( 1000 * myField.GetTransversalComponent( p2, d1))
+b3 = int( 1000 * myField.GetTransversalComponent( p3, d1))
+b4 = int( 1000 * myField.GetTransversalComponent( p4, d1))
+
+print "Evaluating field volume Bykovskiy_201906.dat centered at (0,0,0)",
+if( b1 != 2007 or b2 != 1998 or b3 != 1998 or b4 != 1119 ):
+    print "\nEvaluation of field failed! Exit code : 101"
+    exit(101)
+print "[\033[92m OK \x1b[0m]"
+print ""
+print "All tests passed!"
+
+exit(0)
