28482a2190bde54cd10fa56eb722e250d5a0b687
Javier Galan <j.a.galan.81@gmail.com>
2020-03-08 17:19:32 +0100

    TRestAxionMagneticField. Added official headers.

--- inc/TRestAxionMagneticField.h
+++ inc/TRestAxionMagneticField.h
@@ -1,18 +1,24 @@
-///______________________________________________________________________________
-///______________________________________________________________________________
-///______________________________________________________________________________
-///
-///
-///             RESTSoft : Software for Rare Event Searches with TPCs
-///
-///             myMetadata.h
-///
-///             Metadata class to be used to store basic info
-///             inherited from TRestMetadata
-///
-///             jun 2016:   First concept. Javier Galan
-//
-///_______________________________________________________________________________
+/*************************************************************************
+ * This file is part of the REST software framework.                     *
+ *                                                                       *
+ * Copyright (C) 2016 GIFNA/TREX (University of Zaragoza)                *
+ * For more information see http://gifna.unizar.es/trex                  *
+ *                                                                       *
+ * REST is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by  *
+ * the Free Software Foundation, either version 3 of the License, or     *
+ * (at your option) any later version.                                   *
+ *                                                                       *
+ * REST is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ * GNU General Public License for more details.                          *
+ *                                                                       *
+ * You should have a copy of the GNU General Public License along with   *
+ * REST in $REST_PATH/LICENSE.                                           *
+ * If not, see http://www.gnu.org/licenses/.                             *
+ * For the list of contributors see $REST_PATH/CREDITS.                  *
+ *************************************************************************/
 
 #ifndef _TRestAxionMagneticField
 #define _TRestAxionMagneticField
@@ -27,7 +33,7 @@
 
 #include "TRestMesh.h"
 
-/// A class to load magnetic field maps and provide an easy access to it.
+/// A class to load magnetic field maps and provide an evaluate the field including interpolation.
 class TRestAxionMagneticField : public TRestMetadata {
    private:
     struct MagneticFieldVolume {
