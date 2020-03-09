b1d32ac61ec967712de08748d13199df6b2b2084
Javier Galan <j.a.galan.81@gmail.com>
2020-03-09 17:28:29 +0100

    TRestAxionFieldPropagationProcess. Added a first documentation version.

--- src/TRestAxionFieldPropagationProcess.cxx
+++ src/TRestAxionFieldPropagationProcess.cxx
@@ -21,20 +21,54 @@
  *************************************************************************/
 
 //////////////////////////////////////////////////////////////////////////
-/// TRestAxionFieldPropagationProcess TOBE documented
 ///
-/// The axion is generated with intensity proportional to g_ag = 1.0 x g10
+/// This process is designed to work with the most general case of the magnetic field description using the
+/// help of TRestAxionMagneticField metadata class. This means that the magnetic ﬁeld volume can be made of
+/// several magnetic ﬁeld "regions" (as described in the documentation of TRestAxionMagneticField, where each
+/// "region" is deﬁned by a separate `<addMagnetVolume...>` line in its RML conﬁguration ﬁle. One such example
+/// of generic magnetic ﬁeld volume description is shown in the following figure, where the volume consists of
+/// four regions labeled #1 . . . #4. In the following example the regions appear as connected regions
+/// although in general they could be completely unconnected, or isolated regions. Anywhere where no region is
+/// defined the field will be equal to zero.
+///
+/// \htmlonly <style>div.image img[src="AxionMagnetTrajectory.png"]{width:500px;}</style> \endhtmlonly
+///
+/// ![Schematic of the axion trajectory through the magnetic field volumes.](AxionMagnetTrajectory.png)
+///
+/// Here, the boundaries of each region are represented by blue lines. Also, in some (or all) regions the
+/// magnetic ﬁeld can be zero in its outer parts in order to define a more complex field shape inside.
+/// These parts where \f$B = 0\f$ are shown in blue, while the parts of the
+/// regions with \f$B \neq 0\f$ are shown in green. In this case, it is possible that the particle, right
+/// after entering the region, passes through the part where \f$B = 0\f$ before traversing the section with
+/// \f$B \neq 0\f$. Also, just before exiting the region, the particle can again pass through the section
+/// with \f$B = 0\f$.
+///
+/// This process will use the profile of the transversal magnetic field component at each of the volumes
+/// along the path to calculate the probability the particle is in a photon state at the end of the
+/// trajectory,
+///
+/// In a first approach this process will be only valid for the axion propagation inside a single magnetic
+/// volume, until it is confirmed the process is valid for any number of volumes.
+///
 ///--------------------------------------------------------------------------
 ///
 /// RESTsoft - Software for Rare Event Searches with TPCs
 ///
 /// History of developments:
 ///
-/// 2019-March:  First implementation of shared memory buffer to rawsignal conversion.
-///             Javier Galan
+/// 2019-March:  First prototyping of the process.
+///              Javier Galan
+///
+/// 2019-July:   Implementation of boundaries and magnetic field evaluation.
+///              Eve Pachoud
+///
+/// 2020-March:  Review and validation of this process.
+///              Javier Galan and Krešimir Jakovčić
+///
 ///
 /// \class      TRestAxionFieldPropagationProcess
-/// \author     Javier Galan
+/// \author     Javier Galan <javier.galan@unizar.es>
+/// \author     Krešimir Jakovčić <kjakov@irb.hr>
 ///
 /// <hr>
 ///
@@ -276,6 +310,7 @@ else
             */
     return boundaries;
 }
+
 std::vector<TVector3> TRestAxionFieldPropagationProcess::FieldBoundary(std::vector<TVector3> boundaries,
                                                                        Double_t minStep) {
     std::vector<TVector3> boundariesField;
