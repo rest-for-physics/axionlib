9e3dee03384e11e67c35a44f7f54eacc1be26cfd
Javier Galan <javier.galan@unizar.es>
2022-04-07 12:04:41 +0200

    Updating validation pipeline

--- pipeline/metadata/optics/mirrors.py
+++ pipeline/metadata/optics/mirrors.py
@@ -5,6 +5,7 @@ import ROOT
 ROOT.gSystem.Load("libRestFramework.so")
 ROOT.gSystem.Load("libRestAxion.so")
 
+print( "Loading default dummy mirror" )
 mirror = ROOT.TRestAxionOpticsMirror("mirrors.rml", "default")
 mirror.PrintMetadata()
 
@@ -17,6 +18,7 @@ ref1int = int(10000. * ref1 )
 ref2int = int(10000. * ref2 )
 ref3int = int(10000. * ref3 )
 ref4int = int(10000. * ref4 )
+
 print( "Reflectivity: " + str(ref1int), end ="" )
 if( ref1int != 9721 ):
     print( "\nError! Reflectivity should be 0.9721!" )
@@ -30,7 +32,7 @@ if( ref2int != 9563 ):
 print (" [\033[92m OK \x1b[0m]")
 
 print( "Reflectivity: " + str(ref3int), end ="" )
-if( ref3int != 37 ):
+if( ref3int != 34 ):
     print( "\nError! Reflectivity should be 0.0037!" )
     exit(1)
 print (" [\033[92m OK \x1b[0m]")
@@ -41,6 +43,44 @@ if( ref4int != 2 ):
     exit(1)
 print (" [\033[92m OK \x1b[0m]")
 
+
+print( "Loading XMM mirror" )
+mirror = ROOT.TRestAxionOpticsMirror("mirrors.rml", "XMM")
+mirror.PrintMetadata()
+
+ref1  = mirror.GetReflectivity( 0.25, 1 )
+ref2  = mirror.GetReflectivity( 0.5, 3 )
+ref3  = mirror.GetReflectivity( 0.75, 5 )
+ref4  = mirror.GetReflectivity( 1., 7 )
+
+ref1int = int(10000. * ref1 )
+ref2int = int(10000. * ref2 )
+ref3int = int(10000. * ref3 )
+ref4int = int(10000. * ref4 )
+
+print( "Reflectivity: " + str(ref1int), end ="" )
+if( ref1int != 9428 ):
+    print( "\nError! Reflectivity should be 0.9429!" )
+    exit(1)
+print (" [\033[92m OK \x1b[0m]")
+
+print( "Reflectivity: " + str(ref2int), end ="" )
+if( ref2int != 7099 ):
+    print( "\nError! Reflectivity should be 0.7090!" )
+    exit(1)
+print (" [\033[92m OK \x1b[0m]")
+
+print( "Reflectivity: " + str(ref3int), end ="" )
+if( ref3int != 5357 ):
+    print( "\nError! Reflectivity should be 0.5357!" )
+    exit(1)
+print (" [\033[92m OK \x1b[0m]")
+
+print( "Reflectivity: " + str(ref4int), end ="" )
+if( ref4int != 145 ):
+    print( "\nError! Reflectivity should be 0.0145!" )
+    exit(1)
+print (" [\033[92m OK \x1b[0m]")
 
 print ("All tests passed!")
 
