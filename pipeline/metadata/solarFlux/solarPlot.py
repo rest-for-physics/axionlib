c498b0b05fc05b46b51b3e73a8d08694b13cb25f
Javier Galan <javier.galan@unizar.es>
2022-04-20 15:59:55 +0200

    Updating solarFlux scripts and validation

--- pipeline/metadata/solarFlux/solarPlot.py
+++ pipeline/metadata/solarFlux/solarPlot.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-totalSamples = 20000
-
-outfname = "flux.png"
-
 import math
 import ROOT
 from ROOT import (
@@ -13,6 +9,36 @@ from ROOT import (
     TGraph, TGraph2D,
     TF1, TF2, TF3, TFormula,
     TLorentzVector, TVector3)
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--rml', dest='rmlfile', type=str, help='The input RML file .rml')
+parser.add_argument('--out', dest='outfname', type=str, help='The output filename in png,pdf,C or any other ROOT accepted format')
+parser.add_argument('--fluxname', dest='fluxname', type=str, help='The name of the flux definition to be chosen from the RML')
+parser.add_argument('--N', dest='samples', type=int, help='The number of generated particles')
+args = parser.parse_args()
+
+rmlfile = "fluxes.rml"
+if args.rmlfile != None:
+    rmlfile = args.rmlfile
+
+outfname = "flux.png"
+if args.outfname != None:
+    outfname = args.outfname
+
+fluxname = "combined"
+if args.fluxname != None:
+    fluxname = args.fluxname
+
+
+samples = 20000
+if args.samples != None:
+    samples = args.samples
+
+validation = False
+if rmlfile == "fluxes.rml" and fluxname == "combined" and outfname == "flux.png" and samples == 20000:
+    validation = True
 
 ROOT.gSystem.Load("libRestFramework.so")
 ROOT.gSystem.Load("libRestAxion.so")
@@ -25,7 +51,7 @@ pad1 = TPad("pad1","This is pad1",0.01,0.02,0.99,0.97);
 pad1.Divide(2,2)
 pad1.Draw()
 
-combinedFlux = ROOT.TRestAxionSolarFlux("fluxes.rml", "combined")
+combinedFlux = ROOT.TRestAxionSolarFlux(rmlfile, fluxname)
 combinedFlux.LoadTables()
 combinedFlux.PrintMetadata()
 
@@ -35,19 +61,23 @@ if combinedFlux.GetError():
     exit(101)
 
 comb_spt = TH2D("comb_spt", "Energy versus solar radius", 200, 0, 20, 100, 0, 1 )
-for x in range(totalSamples):
+for x in range(samples):
     x = combinedFlux.GetRandomEnergyAndRadius()
     comb_spt.Fill( x[0], x[1] )
 
 rnd = TRandom3(0)
 solarDisk = TH2D("solar_disk", "SolarDisk", 120, -1.2, 1.2, 120, -1.2, 1.2 )
-for x in range(totalSamples):
+for x in range(samples):
     angle = rnd.Rndm() * 2 * math.pi
     x = combinedFlux.GetRandomEnergyAndRadius()
 
     solarDisk.Fill( x[1]*math.cos(angle), x[1]*math.sin(angle) )
 
 pad1.cd(1)
+pad1.cd(1).SetRightMargin(0.09);
+pad1.cd(1).SetLeftMargin(0.15);
+pad1.cd(1).SetBottomMargin(0.15);
+
 comb_spt.SetStats(0)
 comb_spt.GetXaxis().SetTitle("Energy [keV]")
 comb_spt.GetXaxis().SetTitleSize(0.05);
@@ -58,26 +88,40 @@ comb_spt.GetYaxis().SetLabelSize(0.05);
 comb_spt.Draw("box")
 
 pad1.cd(2)
+pad1.cd(2).SetLogy()
+pad1.cd(2).SetRightMargin(0.09);
+pad1.cd(2).SetLeftMargin(0.15);
+pad1.cd(2).SetBottomMargin(0.15);
 enSpt = comb_spt.ProjectionX()
 enSpt.SetTitle("Energy spectrum")
 enSpt.GetYaxis().SetTitleSize(0.05);
 enSpt.SetStats(0)
 enSpt.Draw()
-if ( enSpt.GetMaximumBin() != 41 ):
-    print ( "\nMaximum Bin is not the expected one! Exit code : 1" )
-    exit(1)
+
+if validation:
+    if ( enSpt.GetMaximumBin() != 41 ):
+        print ( "\nMaximum Bin is not the expected one! Exit code : 1" )
+        exit(1)
 
 pad1.cd(3)
+pad1.cd(3).SetRightMargin(0.09);
+pad1.cd(3).SetLeftMargin(0.15);
+pad1.cd(3).SetBottomMargin(0.15);
 rSpt = comb_spt.ProjectionY()
 rSpt.SetTitle("Radial distribution")
 rSpt.GetYaxis().SetTitleSize(0.05);
 rSpt.SetStats(0)
 rSpt.Draw()
-if ( rSpt.GetMaximumBin() != 25 ):
-    print ( "\nMaximum Bin is not the expected one! Exit code : 2" )
-    exit(2)
+
+if validation:
+    if ( rSpt.GetMaximumBin() != 25 ):
+        print ( "\nMaximum Bin is not the expected one! Exit code : 2" )
+        exit(2)
 
 pad1.cd(4)
+pad1.cd(4).SetRightMargin(0.09);
+pad1.cd(4).SetLeftMargin(0.15);
+pad1.cd(4).SetBottomMargin(0.15);
 solarDisk.SetStats(0)
 solarDisk.GetXaxis().SetTitle("X")
 solarDisk.GetXaxis().SetTitleSize(0.05);
@@ -89,39 +133,6 @@ solarDisk.GetYaxis().SetLabelSize(0.05);
 solarDisk.Draw()
 
 c1.Print(outfname)
-
-monoFlux = ROOT.TRestAxionSolarFlux("fluxes.rml", "mono")
-monoFlux.LoadTables()
-monoFlux.PrintMetadata()
-
-if monoFlux.GetError():
-    print ( monoFlux.GetErrorMessage() )
-    print ( "\nSolar flux initialization failed! Exit code : 101" )
-    exit(101)
-
-x = monoFlux.GetRandomEnergyAndRadius()
-if ( int( x[0]*100 ) != 800 or int( x[1]*100 ) != 58 ):
-    print ( "\nMonochromatic flux values seem to be wrong! Exit code : 201" )
-    exit(201)
-    
-print ("[\033[92m OK \x1b[0m]")
-
-continuumFlux = ROOT.TRestAxionSolarFlux("fluxes.rml", "Gianotti")
-continuumFlux.LoadTables()
-continuumFlux.PrintMetadata()
-
-if continuumFlux.GetError():
-    print ( continuumFlux.GetErrorMessage() )
-    print ( "\nSolar flux initialization failed! Exit code : 101" )
-    exit(101)
-
-x = monoFlux.GetRandomEnergyAndRadius()
-if ( int( x[0]*100 ) != 400 or int( x[1]*100 ) != 24 ):
-    print ( "\nMonochromatic flux values seem to be wrong! Exit code : 202" )
-    exit(202)
-
-print ("All tests passed!  [\033[92m OK \x1b[0m]")
-
-print ("")
+print( "Generated file : " + outfname )
 
 exit(0)
---
+++ pipeline/metadata/solarFlux/solarTests.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import math
+import ROOT
+from ROOT import (
+    TChain, TFile, TTree, TCanvas, TPad, TRandom3,
+    TH1D, TH2D, TH3D,
+    TProfile, TProfile2D, TProfile3D,
+    TGraph, TGraph2D,
+    TF1, TF2, TF3, TFormula,
+    TLorentzVector, TVector3)
+
+
+ROOT.gSystem.Load("libRestFramework.so")
+ROOT.gSystem.Load("libRestAxion.so")
+
+monoFlux = ROOT.TRestAxionSolarFlux("fluxes.rml", "mono")
+monoFlux.LoadTables()
+monoFlux.PrintMetadata()
+
+if monoFlux.GetError():
+    print ( monoFlux.GetErrorMessage() )
+    print ( "\nSolar flux initialization failed! Exit code : 101" )
+    exit(101)
+
+x = monoFlux.GetRandomEnergyAndRadius()
+print( x[0] )
+print( x[1] )
+if ( int( x[0]*100 ) != 800 or int( x[1]*100 ) != 83 ):
+    print ( "\nMonochromatic flux values seem to be wrong! Exit code : 201" )
+    exit(201)
+    
+print ("[\033[92m OK \x1b[0m]")
+
+continuumFlux = ROOT.TRestAxionSolarFlux("fluxes.rml", "Gianotti")
+continuumFlux.LoadTables()
+continuumFlux.PrintMetadata()
+
+if continuumFlux.GetError():
+    print ( continuumFlux.GetErrorMessage() )
+    print ( "\nSolar flux initialization failed! Exit code : 101" )
+    exit(101)
+
+x = monoFlux.GetRandomEnergyAndRadius()
+if ( int( x[0]*100 ) != 400 or int( x[1]*100 ) != 24 ):
+    print ( "\nContinuum flux values seem to be wrong! Exit code : 202" )
+    exit(202)
+
+print ("All tests passed!  [\033[92m OK \x1b[0m]")
+
+print ("")
+
+exit(0)
