38babe91c45fe743d0712f77b351958602ee4b70
Javier Galan <j.a.galan.81@gmail.com>
2024-02-14 21:21:41 +0100

    TRestAxionMagneticField::SetTrack. Adding exit condition when fTrackLenght is zero

--- src/TRestAxionMagneticField.cxx
+++ src/TRestAxionMagneticField.cxx
@@ -1209,6 +1209,7 @@ void TRestAxionMagneticField::SetTrack(const TVector3& position, const TVector3&
         fTrackStart = TVector3(0, 0, 0);
         fTrackDirection = TVector3(0, 0, 0);
         fTrackLength = 0;
+		return;
     }
 
     fTrackStart = trackBounds[0];
