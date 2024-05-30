2d3fc71b90623c37143b9b3d15667492fd258fd8
Javier Galan <javier.galan@unizar.es>
2024-05-30 15:03:56 +0200

    GenerateSignalComponents.C now allows to define the first N steps with a fixed exposure time

--- examples/03.IAXO/GenerateSignalComponents.C
+++ examples/03.IAXO/GenerateSignalComponents.C
@@ -2,7 +2,7 @@ Double_t Eo = 0.5;  // keV
 Double_t Ef = 10;   // keV
 
 Int_t GenerateSignalComponents(std::string rmlFile, std::string name,
-                               Double_t totalExposureTime = 1.5 * 300 * 12 * 3600, size_t skipSteps = 1) {
+                               Double_t totalExposureTime = 1.5 * 300 * 12 * 3600, size_t firstSteps = 5, Double_t firstStepsExposure = 0.5*300*12*3600) {
     TRestAxionField field;
     std::vector<std::pair<Double_t, Double_t>> scanSteps =
         field.GetMassDensityScanning("He", 0.25, 20);  // Up to 0.25 eV
@@ -20,7 +20,7 @@ Int_t GenerateSignalComponents(std::string rmlFile, std::string name,
 
     TFile* f = TFile::Open((TString)componentsFile, "RECREATE");
     std::vector<Double_t> exposureTimes;
-    for (size_t n = skipSteps; n < scanSteps.size(); n++) {
+    for (size_t n = 0; n < scanSteps.size(); n++) {
         Double_t mass = scanSteps[n].first;
         Double_t density = scanSteps[n].second;
 
@@ -30,7 +30,11 @@ Int_t GenerateSignalComponents(std::string rmlFile, std::string name,
 
         Double_t nGamma = gasPhase.GetSignalRate(mass, Eo, Ef);
         Double_t ksvzFactor = 3.75523 * mass;
-        Double_t exposureTime = TMath::Log(20.) / TMath::Power(ksvzFactor, 4) / nGamma;
+        Double_t exposureTime = 0;
+		if( n < firstSteps )
+			exposureTime = firstStepsExposure/firstSteps;
+		else
+			exposureTime = TMath::Log(20.) / TMath::Power(ksvzFactor, 4) / nGamma;
         exposureTimes.push_back(exposureTime);
 
         gasPhase.Write("P" + (TString)IntegerToString(n + 1));
@@ -40,12 +44,15 @@ Int_t GenerateSignalComponents(std::string rmlFile, std::string name,
     std::cout << "Generated signals file: " << componentsFile << std::endl;
 
     Double_t generatedExposureTime = 0;
-    for (size_t n = skipSteps; n < scanSteps.size(); n++)
-        generatedExposureTime += exposureTimes[n - skipSteps];
+    for (size_t n = firstSteps; n < scanSteps.size(); n++)
+        generatedExposureTime += exposureTimes[n];
 
     FILE* g = fopen(settingsFile.c_str(), "wt");
-    for (size_t n = skipSteps; n < scanSteps.size(); n++)
-        fprintf(g, "%lf\tP%d\n", totalExposureTime * exposureTimes[n - skipSteps] / generatedExposureTime,
+    for (size_t n = 0; n < firstSteps; n++)
+        fprintf(g, "%lf\tP%d\n", exposureTimes[n], (int)(n + 1));
+
+    for (size_t n = firstSteps; n < scanSteps.size(); n++)
+        fprintf(g, "%lf\tP%d\n", (totalExposureTime-firstStepsExposure) * exposureTimes[n] / generatedExposureTime,
                 (int)(n + 1));
     fclose(g);
 
